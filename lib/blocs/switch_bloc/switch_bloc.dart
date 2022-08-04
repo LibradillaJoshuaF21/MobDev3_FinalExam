256eee4cd0f195557e67e91ff5bbad58e0e3dd36
Joshua Libradilla <joshlibra52tm@gmail.com>
2022-08-04 17:37:35 +0800

    Implemented switch_bloc

--- lib/blocs/switch_bloc/switch_bloc.dart
+++ lib/blocs/switch_bloc/switch_bloc.dart
@@ -1,13 +1,20 @@
-import 'package:bloc/bloc.dart';
+import 'package:bloc_finals_exam/blocs/bloc_exports.dart';
 import 'package:equatable/equatable.dart';
 
 part 'switch_event.dart';
 part 'switch_state.dart';
 
 class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
-  SwitchBloc() : super(SwitchInitial()) {
+  SwitchBloc() : super(const SwitchInitial(switchValue: false)) {
     on<SwitchEvent>((event, emit) {
-      // TODO: implement event handler
+      emit(
+        const SwitchState(switchValue: true),
+      );
+    });
+    on<SwitchEvent>((event, emit) {
+      emit(
+        const SwitchState(switchValue: false),
+      );
     });
   }
 }
