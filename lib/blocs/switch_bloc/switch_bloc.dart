ee278e69d2dfa0d4076a1dac28abd9d6b101b8c5
Joshua Libradilla <joshlibra52tm@gmail.com>
2022-08-04 18:53:14 +0800

    Implemented HydratedBloc to switchBloc

--- lib/blocs/switch_bloc/switch_bloc.dart
+++ lib/blocs/switch_bloc/switch_bloc.dart
@@ -4,7 +4,7 @@ import 'package:equatable/equatable.dart';
 part 'switch_event.dart';
 part 'switch_state.dart';
 
-class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
+class SwitchBloc extends HydratedBloc<SwitchEvent, SwitchState> {
   SwitchBloc() : super(const SwitchInitial(switchValue: false)) {
     on<SwitchOnEvent>((event, emit) {
       emit(const SwitchState(switchValue: true));
@@ -13,5 +13,15 @@ class SwitchBloc extends Bloc<SwitchEvent, SwitchState> {
     on<SwitchOffEvent>((event, emit) {
       emit(const SwitchState(switchValue: false));
     });
+  }
+
+  @override
+  SwitchState? fromJson(Map<String, dynamic> json) {
+    return SwitchState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(SwitchState state) {
+    return state.toMap();
   }
 }
